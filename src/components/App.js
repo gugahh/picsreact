3d80c152493065029329a5a24c0c4a85a66ada91
Gustavo Santos <gugahh.br@gmail.com>
2020-09-20 17:15:49 -0300

    Commit inicial

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>Hello There!<br/>General Kenobi</p>
+                <p>Ah, the negotiator!</p>
+            </div>
+        );
+    }
+
+}
+
+export default App;
