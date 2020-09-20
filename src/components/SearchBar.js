33c3213c4224ee55386c03c6c052aee60692e4ce
Gustavo Santos <gugahh.br@gmail.com>
2020-09-20 20:30:48 -0300

    Prevent de Submit e Valor Default

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    
-    onInputChange(event) {
-        console.log(event.target.value);
+
+    state = { term: 'Digite Aqui'};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
+    onFocus = () => {
+        if (this.state.term === 'Digite Aqui') {
+            this.setState({term: ''});
+        }
     }
 
     render() {
         return (
             <div className="ui segment" style={{backgroundColor: 'burlywood'}}>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label htmlFor="mysearch">Image Search</label>
                         <input type="text" 
                             id="mysearch"
-                            onChange={this.onInputChange} />
+                            onFocus={this.onFocus}
+                            onChange={(e) => this.setState({term: e.target.value})}
+                            value={this.state.term} />
                     </div>
                 </form>
             </div>
