29e8c2b1e6b559d04affa92e5a3a51bc28abfa7f
Gustavo Santos <gugahh.br@gmail.com>
2020-10-11 12:34:26 -0300

    Começando a usar React Refs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 class ImageCard extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        //console.log(this.imageRef.current);
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
 
     render() {
 
@@ -10,6 +25,7 @@ class ImageCard extends React.Component {
         return ( 
                 <div>
                     <img 
+                        ref={this.imageRef}
                         key={this.props.id}
                         src={urls.regular} 
                         alt={description} />
